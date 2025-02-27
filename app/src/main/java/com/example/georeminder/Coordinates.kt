d42bf8db49961f5883785d8a84199ea1cb6584fe
Yannis Kuenzler <y.kuenzler@icloud.com>
2025-02-27 13:35:24 +0100

    feat: add entities for task and coordinates

---
+++ app/src/main/java/com/example/georeminder/Coordinates.kt
@@ -0,0 +1,13 @@
+package com.example.georeminder
+
+class Coordinates(private var lat: Float, private var lon: Float) {
+
+    public fun getLat(): Float {
+        return this.lat
+    }
+
+    public fun getLon(): Float {
+        return this.lon
+    }
+
+}
