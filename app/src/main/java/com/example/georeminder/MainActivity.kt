6843641b685e5d2cf0b2c0460eeb68c685ef3d93
Yannis Kuenzler <y.kuenzler@icloud.com>
2025-02-27 13:13:22 +0100

    feat: add navigation to create activity

--- app/src/main/java/com/example/georeminder/MainActivity.kt
+++ app/src/main/java/com/example/georeminder/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.georeminder
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 
@@ -9,5 +11,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val createButton = findViewById<Button>(R.id.createButton)
+        createButton.setOnClickListener() {onCreate()}
+    }
+
+    private fun onCreate() {
+        val intent = Intent(this, CreateActivity::class.java)
+        startActivity(intent)
     }
 }
